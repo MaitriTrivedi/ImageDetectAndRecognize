e6873ff5c3969e6ea31573bdcb6520a065d5d271
MaitriTrivedi <74200381+MaitriTrivedi@users.noreply.github.com>
2023-06-03 11:35:42 +0530

    fix : Added comments for better understanding in main.py

--- SelfAttendence/main.py
+++ SelfAttendence/main.py
@@ -4,12 +4,17 @@ from pathlib import Path
 from datetime import date
 import time
 
+# Give path to known images folder
 known_images_files_path = [os.path.abspath("known_faces/"+x) for x in os.listdir("known_faces/")]
-print(known_images_files_path)
 
+# To create excel file for daily report with name as date
 current_date = str(date.today())+".xlsx"
+
+# Give path of the folder where you want to store the daily attendence report 
 attendence_report_files_path = [os.path.abspath("AttendenceReport/"+x) for x in os.listdir("AttendenceReport/")]
 
+# If excel sheet of particular day is not there then it will be generated 
+# Else if it is already there then it will be used to store attendence of that particular day
 if not any(current_date in file_path for file_path in attendence_report_files_path):
     file_name = str(current_date)
     f = open("AttendenceReport/"+file_name, "x")
@@ -18,6 +23,9 @@ if not any(current_date in file_path for file_path in attendence_report_files_pa
     d1 = "DATE"
     f.writelines(f'{name},{current_time},{d1}')
 
+# In unknown_image_file you need to add image of the whose attendence you want to be added
+# It can be added through live video or any other way you want 
+# For that you need to change the input for unknown_image_file
 unknown_image_file = "unknown_faces/einstien.jpeg"
 
 for i in known_images_files_path :
